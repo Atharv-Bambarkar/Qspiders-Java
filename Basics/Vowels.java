589ab78750982dc466a2f11086f0987caeb755dd
Atharv Bambarkar <atharvbambarkar@gmail.com>
2024-01-25 10:28:38 +0530

    done with Decision  making statements

---
+++ Basics/Vowels.java
@@ -0,0 +1,111 @@
+
+public class Vowels {
+    public static void main(String[] args) {
+        //grade(59);
+        //typeOfData('P');
+        switchCase(5);
+
+    }
+
+    static void divisibleBy3And7(int num) {
+        if (num % 3 == 0 && num % 7 == 0) {
+            System.out.println("Divisible by 3 & 7");
+        } else if (num % 3 == 0) {
+            System.out.println("Divisible by 3");
+        } else if (num % 7 == 0) {
+            System.out.println("Divisible by 7");
+        } else {
+            System.out.println("Not Divisible by 3 & 7");
+        }
+
+    }
+
+    static void isVowelOrConsonant(char ch) {
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I'
+                || ch == 'O' || ch == 'U') {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
+    }
+
+
+    static void grade(int marks) {
+        if (marks <= 34) {
+            System.out.println("F");
+        } else if (marks >= 35 && marks <= 49) {
+            System.out.println("D");
+        } else if (marks >= 50 && marks <= 59) {
+            System.out.println("C");
+        } else if (marks >= 60 && marks <= 69) {
+            System.out.println("B");
+        } else if (marks >= 70 && marks <= 79) {
+            System.out.println("B+");
+        } else if (marks >= 80 && marks <= 89) {
+            System.out.println("A");
+        } else if (marks >= 90 && marks <= 100) {
+            System.out.println("A+");
+        } else {
+            System.out.println("Invalid marks");
+        }
+    }
+
+
+
+    static void typeOfData(char ch){
+        if (ch>='A' && ch<='Z'){
+            if (ch == 'A' || ch == 'E' || ch == 'I'
+            || ch == 'O' || ch == 'U') {
+        System.out.println("Upper Vowel");
+    } else {
+        System.out.println("Upper Consonant");
+    }
+
+        }
+
+        else if (ch>='a' && ch<='z'){
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ) {
+            System.out.println("Lower Vowel");
+        } else {
+            System.out.println("Upper Consonant");
+        }
+        }
+
+        else if (ch>='0' && ch<='9'){
+            System.out.println("number");
+        }
+
+        else {
+            System.out.println("special char");
+        }
+
+    }
+
+    static void switchCase(int ch){
+        switch (ch){
+            case 1: System.out.println("Monday");
+            break; // control transfer to next case
+            case 2: System.out.println("Tuesday");
+            break;
+            case 3: System.out.println("Wednesday");
+            break;
+            case 4: System.out.println("Thursday");
+            break;
+            case 5: System.out.println("Friday");
+            break;
+            case 6: System.out.println("Saturday");
+            break;
+            case 7: System.out.println("Sunday");
+            break;
+            default: System.out.println("Invalid day");
+            break;
+            
+        }
+    }
+
+
+
+
+
+
+}
